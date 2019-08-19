36a19b9240784ad64a47c87cf065532782f2bfdb
福島純 <fukushimajun@fukushimajun-no-MacBook-Air.local>
2019-08-19 15:35:55 +0700

    Add Static Pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
-class UsersController < ApplicationController
+class UsersController < ApplicationController 
   def new
     @user = User.new 
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
+      redirect_to @user
     else
       render :new
     end
@@ -14,6 +19,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confimation)
+    params.require(:user).permit(:name,:email,:password,:password_confimation)
   end
 end

824edba3270d766ec840fc4d997be13f5581600c
福島純 <fukushimajun@fukushimajun-no-MacBook-Air.local>
2019-08-21 12:30:00 +0700

    Add Static Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
@@ -15,5 +9,6 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users
+  resources :microposts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

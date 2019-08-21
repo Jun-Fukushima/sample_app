025aacebbda2e08b538feaabb64149af4a7b88c8
福島純 <fukushimajun@fukushimajun-no-MacBook-Air.local>
2019-08-21 10:47:58 +0700

    Add Static Pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
-class UsersController < ApplicationController 
+class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
+  def index
+    @users = User.paginate(page: params[:page]) 
+  end
+
   def new
-    @user = User.new 
+    @user = User.new
   end
 
   def show
@@ -37,4 +44,19 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name,:email,:password,:password_confimation)
   end
-end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "ログインしてください"
+      redirect_to root_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    unless current_user?(@user)
+      redirect_to root_url
+    end
+  end
+end 
+
